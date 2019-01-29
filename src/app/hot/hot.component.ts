c453075fd850916668588da0807e25a22dc541f7
Cristian Lucero and James Cho <cristian@>
2019-01-29 14:35:27 -0800

    This commit allows user to create own post on subreddit topic

--- src/app/hot/hot.component.ts
+++ src/app/hot/hot.component.ts
@@ -13,9 +13,19 @@ export class HotComponent {
   constructor(private router: Router){ }
 
  posts: Post[] = hotPosts;
+ display: boolean = false;
 
  goToPostsPage(clickedTopic: Post) {
    this.router.navigate(['hot', clickedTopic.id]);
  };
+
+ submitForm(title: string, user: string, id: number) {
+   let newPost: Post = new Post(title, user, this.posts.length+1, []);
+   this.posts.push(newPost);
+ }
+
+ showForm() {
+  this.display = !this.display;
+}
 
 }
